00fc016b801064280cf7d546fc1e8f21b9914c5f
aalex19 <123383724+aalex19@users.noreply.github.com>
2023-03-21 22:11:50 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -5,6 +5,12 @@ def password_encoder(password):
         new_password += new_char
     return new_password
 
+def password_decoder(password):
+    decoded_pass = ""
+    for i in password:
+        new_char = str((int(i)-3))
+        decoded_pass += new_char
+    return decoded_pass
 
 print("""Menu
 -------------
@@ -21,6 +27,10 @@ while True:
         encoded_password = password_encoder(first_password)
         print("Your password has been encoded and stored!")
         program_on = True
+
+    if option == 2:
+        decoded = password_decoder(encoded_password)
+        print("The encoded password is", encoded_password + ", and the original password is", decoded)
     if option == 3:
         program_on = False
         break
