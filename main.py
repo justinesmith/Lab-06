fa4194b0e720b91d9c87356221e74f52271d1134
justinesmith <123383763+justinesmith@users.noreply.github.com>
2023-03-21 21:07:34 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,14 +6,6 @@ def password_encoder(password):
     return new_password
 
 
-def password_decoder(new_password):
-    old_password = ''
-    for i in new_password:
-        old_char = str((int(i)-3) % 10)
-        old_password += old_char
-    return old_password
-
-
 print("""Menu
 -------------
 1. Encode
@@ -28,10 +20,6 @@ while True:
         first_password = str(input("Please enter your password to encode: "))
         encoded_password = password_encoder(first_password)
         print("Your password has been encoded and stored!")
-        program_on = True
-    if option == 2:
-        decoded_password = password_decoder(str(encoded_password))
-        print(f"The encoded password is {encoded_password}, and the original password is {decoded_password}.")
         program_on = True
     if option == 3:
         program_on = False
